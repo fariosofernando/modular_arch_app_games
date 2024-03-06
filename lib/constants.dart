3bfcdfbb8e93e22d4da3cacd57987beafdc20ebe
fariosofernando <farioso.f@gmail.com>
2024-03-06 14:12:59 +0200

    ref/update: atualizando a arquitetura

--- lib/constants.dart
+++
@@ -1,1 +0,0 @@
-const baseApi = 'https://63f7ba5c833c7c9c608a51a6.mockapi.io/api';
--- lib/models/comment.dart
+++
@@ -1,9 +0,0 @@
-class Comment {
-  String text;
-  DateTime date;
-
-  Comment({
-    required this.text,
-    required this.date,
-  });
-}
